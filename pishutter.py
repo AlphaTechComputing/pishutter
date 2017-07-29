aeeba8d9da9bd84c7e22e3bda50efcc7c74ad8c2
harrypit06 <mkennedy@alphatech-computing.com>
2017-07-29 05:09:22 +0000

    Comparison with local timezone now working.

--- pishutter.py
+++ pishutter.py
@@ -1,1 +1,32 @@
 #!/bin/python
+import datetime
+import time
+import pytz
+import dateutil.parser
+from astral import Astral
+
+# Location variables
+city_name = 'Columbus'
+a = Astral()
+a.solar_depression = 'civil'
+city = a[city_name]
+
+#Time variables
+today =  datetime.datetime.now()
+current_time = datetime.datetime.now(pytz.timezone('US/Eastern'))
+year =  int(today.year)
+month = int(today.strftime("%m"))
+day = int(today.strftime("%d"))
+second = int(today.strftime("%M"))
+sun = city.sun(date=datetime.date(year, month, day), local=True)
+sunrise_datetime = str(sun['sunrise'])
+sunset_datetime = str(sun['sunset'])
+sunrise_parsed = dateutil.parser.parse(str(sunrise_datetime))
+sunset_parsed = dateutil.parser.parse(str(sunset_datetime))
+current_parsed = dateutil.parser.parse(str(current_time))
+
+print sunrise_datetime
+print current_time
+
+if sunrise_parsed >= current_parsed:
+	print "oh dang"
