86f11bffeceecb2555556ac3b782bce6a5f4d176
harrypit06 <mkennedy@alphatech-computing.com>
2017-07-29 05:56:29 +0000

    Logic should be functioning correctly. This script is ready for transfer to a Pi for GPIO programming, and to change the print results of the function to pulling the correct GPIO high or low

--- pishutter.py
+++ pishutter.py
@@ -7,13 +7,14 @@ from astral import Astral
 
 # Location variables
 city_name = 'Columbus'
+timezone = 'US/Eastern'
 a = Astral()
 a.solar_depression = 'civil'
 city = a[city_name]
 
 #Time variables
 today =  datetime.datetime.now()
-current_time = datetime.datetime.now(pytz.timezone('US/Eastern'))
+current_time = datetime.datetime.now(pytz.timezone(timezone))
 year =  int(today.year)
 month = int(today.strftime("%m"))
 day = int(today.strftime("%d"))
@@ -25,8 +26,29 @@ sunrise_parsed = dateutil.parser.parse(str(sunrise_datetime))
 sunset_parsed = dateutil.parser.parse(str(sunset_datetime))
 current_parsed = dateutil.parser.parse(str(current_time))
 
-print sunrise_datetime
-print current_time
 
-if sunrise_parsed >= current_parsed:
-	print "oh dang"
+#print "Sunrise: " + str(sunrise_parsed)
+#print "Sunset: " + str(sunset_parsed)
+#print "Current: " + str(current_parsed)
+
+def main():
+    while True:
+	#before sunrise
+	if sunrise_parsed <= current_parsed >= sunset_parsed:
+		print "after sunrise and before sunset; closing filter"
+		time.sleep(30)
+
+	if sunset_parsed >= current_parsed <= sunrise_parsed:
+                print "after sunset and before sunrise opening filter"
+		time.sleep(30)
+
+	else:
+		print "you screwed it up!"
+		time.sleep(30)
+
+
+
+if __name__ == '__main__':
+    main()
+    time.sleep(1)
+    exit(0)
