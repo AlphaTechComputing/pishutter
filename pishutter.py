ac46ae0062db83ea28b37056c0cf4018c441e5d8
harrypit06 <mkennedy@alphatech-computing.com>
2017-08-01 15:55:10 -0400

    Fixed problem with time variables not being updated by the function.

--- pishutter.py
+++ pishutter.py
@@ -24,23 +24,26 @@ a = Astral()
 a.solar_depression = 'civil'
 city = a[city_name]
 
-#Time variables
-today =  datetime.datetime.now()
-current_time = datetime.datetime.now(pytz.timezone(timezone))
-year =  int(today.year)
-month = int(today.strftime("%m"))
-day = int(today.strftime("%d"))
-second = int(today.strftime("%M"))
-sun = city.sun(date=datetime.date(year, month, day), local=True)
-sunrise_datetime = str(sun['sunrise'])
-sunset_datetime = str(sun['sunset'])
-sunrise_parsed = dateutil.parser.parse(str(sunrise_datetime))
-sunset_parsed = dateutil.parser.parse(str(sunset_datetime))
-current_parsed = dateutil.parser.parse(str(current_time))
-
 
 #Check what filter status needs to be based on current time, sunrise, and sunset
 def return_filter_time():
+
+	#Time variables
+	today =  datetime.datetime.now()
+	current_time = datetime.datetime.now(pytz.timezone(timezone))
+	year =  int(today.year)
+	month = int(today.strftime("%m"))
+	day = int(today.strftime("%d"))
+	second = int(today.strftime("%M"))
+	sun = city.sun(date=datetime.date(year, month, day), local=True)
+
+
+	sunrise_datetime = str(sun['sunrise'])
+	sunset_datetime = str(sun['sunset'])
+	sunrise_parsed = dateutil.parser.parse(str(sunrise_datetime))
+	sunset_parsed = dateutil.parser.parse(str(sunset_datetime))
+	current_parsed = dateutil.parser.parse(str(current_time))
+
 
 	print "Sunrise: " + str(sunrise_parsed)
 	print "Sunset: " + str(sunset_parsed)
